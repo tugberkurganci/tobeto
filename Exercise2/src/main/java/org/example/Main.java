6a5558443c6a954a13401575c82fbebc7b8a7e0e
Tuğberk Urgancı <tugberkurganci@gmail.com>
2023-10-24 17:01:04 +0300

    second

--- Exercise2/src/main/java/org/example/Main.java
+++ Exercise2/src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
-package org.example;
+package main.java.org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product product1 = new Product();
+        product1.setName("Delonghi Kahve Makinesi");
+        product1.setDiscount(7);
+        product1.setUnitPrice(7500);
+        product1.setUnitsInStock(3);
+        product1.setImageUrl("product1.jpg");
+
+        Product product2 = new Product();
+        product2.setName("Smeg Kahve Makinesi");
+        product2.setDiscount(7);
+        product2.setUnitPrice(7500);
+        product2.setUnitsInStock(3);
+        product2.setImageUrl("product2.jpg");
+
+
+
+        Product product3 = new Product();
+        product3.setName("Kitchen Aid Kahve Makinesi");
+        product3.setDiscount(7);
+        product3.setUnitPrice(7500);
+        product3.setUnitsInStock(3);
+        product3.setImageUrl("product3.jpg");
+
+        Product[] products = {product1,product2,product3};
+        for (Product product : products){
+            System.out.println("<li>" + product.getName() + "</li>");
+        }
+        System.out.println("</ul>");
     }
 }
